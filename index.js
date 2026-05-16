bcef28d3129c3be593eb8e9ce0b403c638a8ce4b
Miguel Aviles <miguelaviles@Mac.lan>
2026-05-15 21:33:34 -0500

    YourMessage

---
+++ index.js
@@ -0,0 +1,38 @@
+// https://www.omdbapi.com/?s=marvel&apikey=d7d0b17d
+
+let page = 1;
+
+async function getMovies() {
+  try {
+    const res = await fetch(`https://www.omdbapi.com/?s=marvel&apikey=d7d0b17d&page=${page}`);
+    const data = await res.json();
+    data.Search.forEach(movie => {
+      if (movie.Poster === 'N/A') return;
+      console.log(movie);
+      document.getElementById("movie-list").innerHTML += `
+        <div class="movie-card">
+          <img src="${movie.Poster}" onerror="this.parentElement.remove()" />
+          <h2>${movie.Title}</h2>
+          <p>${movie.imdbRating}</p>
+        </div>
+      `;
+    });
+  } catch (error) {
+    console.log("Something went wrong", error);
+  }
+}
+
+document.getElementById("load-more").addEventListener("click", () => {
+  page++;
+  getMovies();
+});
+getMovies(); 
+
+const hamMenu = document.querySelector(`.ham-menu`);
+
+const offScreenMenu = document.querySelector(`.off-screen-menu`);
+
+hamMenu.addEventListener(`click`, () => {
+  hamMenu.classList.toggle(`active`);
+  offScreenMenu.classList.toggle(`active`);
+})
