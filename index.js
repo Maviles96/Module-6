343d384410bed19587a566bc3e45e73733c201f5
Miguel Aviles <miguelaviles@Mac.lan>
2026-05-16 19:28:43 -0500

    YourMessage

--- index.js
+++ index.js
@@ -4,18 +4,28 @@ let page = 1;
 
 async function getMovies() {
   try {
-    const res = await fetch(`https://www.omdbapi.com/?s=marvel&apikey=d7d0b17d&page=${page}`);
+    const res = await fetch(
+      `https://www.omdbapi.com/?s=marvel&apikey=d7d0b17d&page=${page}`
+    );
+
     const data = await res.json();
-    data.Search.forEach(movie => {
-      if (movie.Poster === 'N/A') return;
-      console.log(movie);
-      document.getElementById("movie-list").innerHTML += `
+
+    data.Search.forEach(async movie => {
+      if (movie.Poster === "N/A") return;
+
+      const detailsRes = await fetch(
+        `https://www.omdbapi.com/?i=${movie.imdbID}&apikey=d7d0b17d`
+      );
+
+      const details = await detailsRes.json();
+
+      `document.getElementById("movie-list").innerHTML += 
         <div class="movie-card">
           <img src="${movie.Poster}" onerror="this.parentElement.remove()" />
           <h2>${movie.Title}</h2>
-          <p>${movie.imdbRating}</p>
+          <p>IMDb: ${details.imdbRating}</p>
         </div>
-      `;
+      ;`
     });
   } catch (error) {
     console.log("Something went wrong", error);
